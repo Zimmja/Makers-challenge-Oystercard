619e768c4159d5a4d28da99c1598f7be581e62fb
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-10-29 12:04:31 +0200

    create Journey class

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,14 +1,5 @@
 require 'journey'
 
 describe Journey do
-  
+
 end
-
-
-
-
-#Should be responsiblefor:
-# - Starting a journey
-# - Finishing a journey
-# - Calculating the fare of a journey
-# - Returning whether or not the previous journey is complete
